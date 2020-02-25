c9f672c621db7cae771be98a0e51f97c0ecfda7a
Viktar Muronchyk <viktar.muronchyk@gmail.com>
2020-02-25 22:40:08 +0300

    setup webpack dev config

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.scss';
+
+const root = document.createElement('div');
+document.querySelector('body').appendChild(root);
+
+ReactDOM.render(
+  <div>
+    <h1>1231231312</h1>
+  </div>,
+  root
+);
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: [path.resolve(__dirname, 'src/index.js')],
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js'
+  },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  resolve: {
+    modules: ['node_modules']
+  },
+  devServer: {
+    contentBase: path.join(__dirname, './dist'),
+    compress: true,
+    port: 8080
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin()]
+};
