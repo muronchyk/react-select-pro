e3bd958f9f09a81365b3712d1eea4a878b4c7c69
Viktar Muronchyk <viktar.muronchyk@gmail.com>
2020-03-26 18:16:56 +0300

    Revert log

--- src/common/index.tsx
+++ src/common/index.tsx
@@ -37,8 +37,6 @@ const SelectPro = ({
 }: SelectProps) => {
     const containerRef = useRef<HTMLDivElement>(null);
 
-    console.log('sample');
-
     const [inputValue, setInputValue] = useState('');
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
